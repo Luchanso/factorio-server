9fd142a3fc388d3ed057d43fa73e0a1fe74bf66e
Oleg Loutchansky <luchansodev@gmail.com>
2018-12-21 22:40:12 +0300

    chore(*): remove old data

--- set-auth.sh
+++ set-auth.sh
@@ -3,5 +3,6 @@
 # File wich set server credentials
 
 sed -i "s/\"token\": \"\"/\"token\": \"$token\"/g" config/server-settings.json
-sed -i "s/\"password\": \"\"/\"password\": \"$password\"/g" config/server-settings.json
+sed -i "s/\"game_password\": \"\"/\"game_password\": \"$password\"/g" config/server-settings.json
+echo $password > config/rconpw
 echo "Done";
